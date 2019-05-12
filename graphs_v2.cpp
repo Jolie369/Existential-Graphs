8eaccdf0dde836d681c5f67d4ae97a6fb4e7dbcc
Jolie369 <50542641+Jolie369@users.noreply.github.com>
2019-05-12 12:12:05 +0300

    Create graphs_v2.cpp

---
+++ graphs_v2.cpp
@@ -0,0 +1,418 @@
+// Copyright 2019 Luca Istrate, Danut Matei
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <utility>
+#include <cassert>
+#include "./aegraph.h"
+
+std::string strip(std::string s) {
+    // deletes whitespace from the beginning and end of the string
+    s.erase(0, s.find_first_not_of(" \n\r\t"));
+    s.erase(s.find_last_not_of(" \n\r\t")+1);
+    return s;
+}
+
+std::pair<std::string, std::string> split_first(std::string s,
+    char delimiter = ',') {
+    // returns a pair that contains: <first_cut, rest_of_graph>
+
+    int numOpen = 0;
+
+    int len = s.size();
+    for (int i = 0; i < len; i++) {
+        char c = s[i];
+        if (c == delimiter && numOpen == 0)
+            return std::make_pair(
+                    strip(s.substr(0, i)), strip(s.substr(i+1, len)));
+        if (c == '[')
+            numOpen += 1;
+        if (c == ']')
+            numOpen -= 1;
+    }
+
+    return {strip(s), std::string()};
+}
+
+
+std::vector<std::string> split_level(std::string s, char delimiter = ',') {
+    // splits 's' into separate entities (atoms, subgraphs)
+
+    std::vector<std::string> result;
+    auto snd = s;
+    while (true) {
+        auto p = split_first(snd, delimiter);
+        auto fst = p.first;
+        snd = p.second;
+
+        result.push_back(fst);
+
+        if (snd.empty())
+            return result;
+    }
+}
+
+
+int AEGraph::num_subgraphs() const {
+    return subgraphs.size();
+}
+
+
+int AEGraph::num_atoms() const {
+    return atoms.size();
+}
+
+
+int AEGraph::size() const {
+    return num_atoms() + num_subgraphs();
+}
+
+
+bool AEGraph::operator<(const AEGraph& other) const {
+    return this->repr() < other.repr();
+}
+
+bool AEGraph::operator==(const AEGraph& other) const {
+    return this->repr() == other.repr();
+}
+
+bool AEGraph::operator!=(const AEGraph& other) const {
+    return this->repr() != other.repr();
+}
+
+AEGraph AEGraph::operator[](const int index) const {
+    // offers an easier way of accessing the nested graphs
+    if (index < num_subgraphs()) {
+        return subgraphs[index];
+    }
+
+    if (index < num_subgraphs() + num_atoms()) {
+        return AEGraph('(' + atoms[index - num_subgraphs()] + ')');
+    }
+
+    return AEGraph("()");
+}
+
+std::ostream& operator<<(std::ostream &out, const AEGraph &g) {
+    out << g.repr();
+    return out;
+}
+
+AEGraph::AEGraph(std::string representation) {
+    // constructor that creates an AEGraph structure from a
+    // serialized representation
+    char left_sep = representation[0];
+    char right_sep = representation[representation.size() - 1];
+
+    assert((left_sep == '(' && right_sep == ')')
+        || (left_sep == '[' && right_sep == ']'));
+
+    // if the left separator is '(' then the AEGraph is the entire
+    // sheet of assertion
+    if (left_sep == '(') {
+        is_SA = true;
+    } else {
+        is_SA = false;
+    }
+
+    // eliminate the first pair of [] or ()
+    representation = representation.substr(1, representation.size() - 2);
+
+    // split the graph into separate elements
+    auto v = split_level(representation);
+    // add them to the corresponding vector
+    for (auto s : v) {
+        if (s[0] != '[') {
+            atoms.push_back(s);
+        } else {
+            subgraphs.push_back(AEGraph(s));
+        }
+    }
+
+    // also internally sort the new graph
+    this->sort();
+}
+
+std::string AEGraph::repr() const {
+    // returns the serialized representation of the AEGraph
+
+    std::string left, right;
+    if (is_SA) {
+        left = '(';
+        right = ')';
+    } else {
+        left = '[';
+        right = ']';
+    }
+
+    std::string result;
+    for (auto subgraph : subgraphs) {
+        result += subgraph.repr() + ", ";
+    }
+
+    int len = atoms.size();
+    if (len != 0) {
+        for (int i = 0; i < len - 1; i++) {
+            result += atoms[i] + ", ";
+        }
+        result += atoms[len - 1];
+    } else {
+        if (subgraphs.size() != 0)
+            return left + result.substr(0, result.size() - 2) + right;
+    }
+
+    return left + result + right;
+}
+
+
+void AEGraph::sort() {
+    std::sort(atoms.begin(), atoms.end());
+
+    for (auto& sg : subgraphs) {
+        sg.sort();
+    }
+
+    std::sort(subgraphs.begin(), subgraphs.end());
+}
+
+bool AEGraph::contains(const std::string other) const {
+    // checks if an atom is in a graph
+    if (find(atoms.begin(), atoms.end(), other) != atoms.end())
+        return true;
+
+    for (const auto& sg : subgraphs)
+        if (sg.contains(other))
+            return true;
+
+    return false;
+}
+
+bool AEGraph::contains(const AEGraph& other) const {
+    // checks if a subgraph is in a graph
+    if (find(subgraphs.begin(), subgraphs.end(), other) != subgraphs.end())
+        return true;
+
+    for (const auto& sg : subgraphs)
+        if (sg.contains(other))
+            return true;
+
+    return false;
+}
+
+std::vector<std::vector<int>> AEGraph::get_paths_to(const std::string other)
+    const {
+    // returns all paths in the tree that lead to an atom like <other>
+    std::vector<std::vector<int>> paths;
+
+    int len_atoms = num_atoms();
+    int len_subgraphs = num_subgraphs();
+
+    for (int i = 0; i < len_atoms; i++) {
+        if (atoms[i] == other && size() > 1) {
+            paths.push_back({i + len_subgraphs});
+        }
+    }
+
+    for (int i = 0; i < len_subgraphs; i++) {
+        if (subgraphs[i].contains(other)) {
+            auto r = subgraphs[i].get_paths_to(other);
+            for (auto& v : r)
+                v.insert(v.begin(), i);
+            copy(r.begin(), r.end(), back_inserter(paths));
+        }
+    }
+
+    return paths;
+}
+
+std::vector<std::vector<int>> AEGraph::get_paths_to(const AEGraph& other)
+    const {
+    // returns all paths in the tree that lead to a subgraph like <other>
+    std::vector<std::vector<int>> paths;
+    int len_subgraphs = num_subgraphs();
+
+    for (int i = 0; i < len_subgraphs; i++) {
+        if (subgraphs[i] == other && size() > 1) {
+            paths.push_back({i});
+        } else {
+            auto r = subgraphs[i].get_paths_to(other);
+            for (auto& v : r)
+                v.insert(v.begin(), i);
+            copy(r.begin(), r.end(), back_inserter(paths));
+        }
+    }
+
+    return paths;
+}
+
+void possibleDCR(AEGraph x, int anterior,
+	std::vector<std::vector<int>>& rez, std::vector<int>& path){
+	if(anterior >= 1 && x.num_subgraphs() == 1 && x.num_atoms() == 0){
+		rez.push_back(path);
+	}
+	if(x.num_subgraphs() >= 1){
+		for(int i = 0; i < x.num_subgraphs(); ++i){
+			path.push_back(i);
+			possibleDCR(x.subgraphs[i], x.num_subgraphs(), rez, path);
+			path.pop_back();
+		}
+	}
+}
+
+std::vector<std::vector<int>> AEGraph::possible_double_cuts() const {
+    // 10p
+    std::vector<std::vector<int>> rez = {};
+    std::vector<int> path = {};
+    AEGraph g(repr());
+    possibleDCR(g, -1, rez, path);
+    return rez;
+}
+
+void copyGraf(AEGraph& x, const AEGraph* y){
+	int ok = 1;
+    for (int i = 0; i < y->num_subgraphs(); ++i){
+    	x.subgraphs.push_back(y->subgraphs[i]);
+    	if(ok){
+    		for(auto i : y->atoms){
+	    		x.atoms.push_back(i);
+	    	}
+	    	ok = 0;
+		}
+    }
+}
+
+void doubleCutR(AEGraph& rez, std::vector<int>& where, int& ok){
+		for(int i = 0; i < rez.num_subgraphs(); ++i){
+			if(where.front() == i){
+		    	if(where.size() > 1){
+		    		where.erase(where.begin());
+		    		doubleCutR(rez.subgraphs[i], where, ok);
+		    	}
+		  		if(ok && where.size() == 1){
+			  		rez.subgraphs[where[0]] =
+			  			rez.subgraphs[where[0]].subgraphs[0];
+					for(auto a : rez.subgraphs[where[0]].atoms){
+						rez.atoms.push_back(a);
+					}
+					rez.subgraphs[where[0]].atoms.clear();
+					std::vector<AEGraph> sub;
+					for(int i = 0; i < rez.num_subgraphs(); i++){
+						if(i != where[0]){
+							sub.push_back(rez.subgraphs[i]);
+						}
+					}
+					rez.subgraphs.clear();
+					rez.subgraphs = sub;
+					ok = 0;
+					doubleCutR(rez, where, ok);
+				}
+			}
+	    }
+}
+
+AEGraph AEGraph::double_cut(std::vector<int> where) const {
+    // 10p
+    AEGraph rez("()");
+    rez.atoms.pop_back();
+    copyGraf(rez, this);
+    int ok = 1;
+    doubleCutR(rez, where, ok);
+    return rez;
+    return AEGraph("()");
+}
+
+void possibleER(AEGraph x, int anterior,
+	std::vector<std::vector<int>>& rez, std::vector<int>& path, int ok){
+    if (path.size() == 1){
+      rez.push_back(path);
+  	} else if (path.size() % 2 == 1 && ok){
+      rez.push_back(path);
+    }
+
+    ok = 0;
+    if (x.size() >= 1){
+  		for(int i = 0; i < x.size(); ++i){
+  			path.push_back(i);
+        if (x.size() > 1) ok = 1;
+        if (i<x.num_subgraphs())
+  			possibleER(x.subgraphs[i], x.num_subgraphs(), rez, path, ok);
+        else if((path.size() % 2 == 1 && ok) || path.size() == 1){
+            rez.push_back(path);
+          }
+  			path.pop_back();
+  		}
+  	}
+  }
+
+std::vector<std::vector<int>> AEGraph::possible_erasures(int level) const {
+    // 10p
+    std::vector<std::vector<int>> rez = {};
+    std::vector<int> path = {};
+    int ok = 1;
+    AEGraph g(repr());
+    possibleER(g, -1, rez, path, ok);
+    return rez;
+
+}
+
+void eraseR(AEGraph& rez, std::vector<int>& where, int& ok){
+	std::cerr << where.front() << " " << where.size() << " " << rez.num_subgraphs() << std::endl;
+	for(int i = 0; i < rez.num_subgraphs(); ++i){
+		if(where.front() == i){
+			if(where.size() >= 1){
+				where.erase(where.begin());
+				std::cerr << rez << std::endl;
+				eraseR(rez.subgraphs[i], where, ok);
+				std::cerr << rez << std::endl;
+			}
+			std::cerr << rez << std::endl;
+		}
+	}
+}
+
+AEGraph AEGraph::erase(std::vector<int> where) const {
+    // 10p
+  AEGraph rez("()");
+  rez.atoms.pop_back();
+  copyGraf(rez, this);
+  int ok = 1;
+  eraseR(rez, where, ok);
+  return rez;
+
+}
+
+void possibleD(AEGraph x, std::vector<std::vector<int>>& rez){
+	std::vector<std::vector<int>> partial_path;
+	std::string at;
+	for(int i = 0; i < x.size(); ++i){
+		if(i < x.num_subgraphs()){
+			partial_path = x.get_paths_to(x[i]);
+		} else {
+			at = x[i].repr();
+			at = at[1];
+			partial_path = x.get_paths_to(at);
+		}
+		for(int j = 0; j < partial_path.size(); ++j){
+			if(partial_path[j][0] != i){
+				rez.push_back(partial_path[j]);
+			}
+		}
+	}
+}
+
+std::vector<std::vector<int>> AEGraph::possible_deiterations() const {
+    // 20p
+    std::vector<std::vector<int>> rez = {};
+    AEGraph g(repr());
+    possibleD(g, rez);
+    return rez;
+}
+
+AEGraph AEGraph::deiterate(std::vector<int> where) const {
+    // 10p
+    return AEGraph("()");
+}
